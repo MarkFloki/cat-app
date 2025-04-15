6ee8e0a559f39aab0c786f5fdf9c3d18f5a5358c
MarkFloki <flokiproject@gmail.com>
2025-04-15 14:16:13 +0300

    ✨ Додано обране, кнопка видалення, UI-покращення

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,34 @@
 import { Component } from '@angular/core';
-import { NgIf } from '@angular/common';
+import { NgIf, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   catUrl: string = '';
+  favorites: string[] = [];
+  showFavorites: boolean = false;
 
   getRandomCat() {
     const timestamp = new Date().getTime();
     this.catUrl = `https://cataas.com/cat?ts=${timestamp}`;
-    console.log('catUrl:', this.catUrl);
+  }
+
+  addToFavorites() {
+    if (this.catUrl && !this.favorites.includes(this.catUrl)) {
+      this.favorites.push(this.catUrl);
+    }
+  }
+
+  removeFromFavorites(cat: string) {
+    this.favorites = this.favorites.filter(fav => fav !== cat);
+  }
+
+  toggleFavorites() {
+    this.showFavorites = !this.showFavorites;
   }
 }
